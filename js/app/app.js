1ea5f4db4b77a22ae1101f32c795974f9520fee3
todaysmathematics <40862648+todaysmathematics@users.noreply.github.com>
2018-07-05 11:19:10 -0400

    comment stale html references

--- js/app/app.js
+++ js/app/app.js
@@ -1,18 +1,18 @@
 define(['jquery', 'moment', 'noget'], function ($, moment, noget) {
 
     $('#todaysmath').hide().fadeIn(300);
-    $('#hour').hide().fadeIn(1000);
-    $('#minute').hide().fadeIn(2000);
-    $('#second').hide().fadeIn(3000);
-    $('#millisecond').hide().fadeIn(1000);
+//    $('#hour').hide().fadeIn(1000);
+//    $('#minute').hide().fadeIn(2000);
+//    $('#second').hide().fadeIn(3000);
+//    $('#millisecond').hide().fadeIn(1000);
 
-    var timeClock = function () {
-        $('#time').text(moment().format('H:mm:ss.SSS'));
-        var time = moment($('#time').text(), "H:mm:ss.SSS");
+//     var timeClock = function () {
+//         $('#time').text(moment().format('H:mm:ss.SSS'));
+//         var time = moment($('#time').text(), "H:mm:ss.SSS");
         
-        var unit = time.format("SSS");
-        $('#millisecond').html(noget.translate(unit)).attr('title', unit);
-    }
+//         var unit = time.format("SSS");
+//         $('#millisecond').html(noget.translate(unit)).attr('title', unit);
+//     }
 
     var mathClock = function () {
 
@@ -23,20 +23,20 @@ define(['jquery', 'moment', 'noget'], function ($, moment, noget) {
         unit = moment().format('D')
         $('#todaysmath').html(noget.translate(unit)).attr('title', unit);
 
-        unit = time.format("H");
-        $('#hour').html(noget.translate(unit)).attr('title', unit);
+//         unit = time.format("H");
+//         $('#hour').html(noget.translate(unit)).attr('title', unit);
 
-        unit = time.format("m");
-        $('#minute').html(noget.translate(unit)).attr('title', unit);
+//         unit = time.format("m");
+//         $('#minute').html(noget.translate(unit)).attr('title', unit);
 
-        unit = time.format("s");
-        $('#second').html(noget.translate(unit)).attr('title', unit);
+//         unit = time.format("s");
+//         $('#second').html(noget.translate(unit)).attr('title', unit);
 
     }
 
-    timeClock();    
+//    timeClock();    
     mathClock();    
-    setInterval(timeClock, 100);
+//    setInterval(timeClock, 100);
     setInterval(mathClock, 1000);
     
 });
