56bccf83819b40c819ca5a8db0ac47689fd0f72a
todaysmathematics <40862648+todaysmathematics@users.noreply.github.com>
2018-07-20 21:37:16 -0400

    tweak clock

--- js/app/app.js
+++ js/app/app.js
@@ -20,6 +20,7 @@ define(['jquery', 'moment', 'noget'], function ($, moment, noget) {
     var mathClock = function () {
 
         $('#date').text(moment().format('M.D.YYYY'));
+        $('#time').text(moment().format('H:mm:ss'));
         // var time = moment($('#time').text(), "H:mm:ss.SSS");
         var time = moment($('#time').text(), "H:mm:ss");
         var unit = 0;
